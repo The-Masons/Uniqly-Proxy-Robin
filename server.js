981a7b1c6f9c3443c82a44151082313d0008e17b
Robin Markowitz <loepoe71@gmail.com>
2018-04-24 19:32:37 -0700

    Add routing

--- server.js
+++ server.js
@@ -9,4 +9,8 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, './public')));
 
+app.get('/product/:productId', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 app.listen(port, () => console.log(`Proxy listening on ${port}`));
