e5dcd80d2f6e9f4948743823224442007041bc42
Robin Markowitz <loepoe71@gmail.com>
2018-04-27 16:50:14 -0700

    Add Axios

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,5 +10,12 @@ app.use(express.static(path.join(__dirname, './public')));
 app.get('/product/:productId', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
+
+app.get('/product/:productId/sizes_qtys', () => {});
+app.get('/product/:productId/addtocart', () => {});
+app.get('/product/:productId/colors', () => {});
+app.get('/product/:productId/images', () => {});
+app.get('/productdetails/:productId', () => {});
+app.get('/reviews/:productId', () => {});
 
 app.listen(port, () => console.log(`Proxy listening on ${port}`));
