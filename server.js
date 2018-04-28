1879afbb629025e59b325b9cb756e2b4065e6548
Robin Markowitz <loepoe71@gmail.com>
2018-04-27 20:16:07 -0700

    Serve proxy server

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
 const axios = require('axios');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, './public')));
+app.use(bodyParser.json());
 
 app.get('/product/:productId', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
@@ -13,14 +15,53 @@ app.get('/product/:productId', (req, res) => {
 
 app.get('/product/:productId/sizes_qtys', (req, res) => {
   axios.get(`http://13.57.205.67:3001/product/${req.params.productId}/sizes_qtys`)
-    .then(data => res.status(200).send(data))
+    .then(resp => {
+      console.log(resp.data);
+      res.status(200).send(resp.data);
+    })
     .catch(err => res.status(500).send(err));
 });
 
-app.get('/product/:productId/addtocart', () => {});
-app.get('/product/:productId/colors', () => {});
-app.get('/product/:productId/images', () => {});
-app.get('/productdetails/:productId', () => {});
-app.get('/reviews/:productId', () => {});
+app.get('/product/:productId/addtocart', (req, res) => {
+  axios.get(`http://13.57.205.67:3001/product/${req.params.productId}/addtocart`)
+  .then(resp => {
+    console.log(resp.data);
+    res.status(200).send(resp.data);
+  })
+    .catch(err => res.status(500).send(err));
+});
+
+app.get('/product/:productId/colors', (req, res) => {
+  axios.get(`http://54.183.208.237:3003/product/${req.params.productId}/colors`)
+  .then(resp => {
+    console.log(resp.data);
+    res.status(200).send(resp.data);
+  })
+    .catch(err => res.status(500).send(err));
+});
+
+app.get('/product/:productId/images', (req, res) => {
+  axios.get(`http://54.183.208.237:3003/product/${req.params.productId}/images`)
+  .then(resp => {
+    console.log(resp.data);
+    res.status(200).send(resp.data);
+  })
+    .catch(err => res.status(500).send(err));
+});
+
+app.get('/productdetails/:productId', (req, res) => {
+  axios.get(`http://54.183.208.237:3003/productdetails/${req.params.productId}`)
+  .then(resp => {
+    console.log(resp.data);
+    res.status(200).send(resp.data);
+  })
+    .catch(err => res.status(500).send(err));
+});
+
+app.get('/reviews/:productId', (req, res) => {
+  axios.get(`http://13.58.109.199:3005/reviews/${req.params.productId}`)
+    .then(resp => res.status(200).send(resp.data))
+    .catch(err => res.status(500).send(err));
+});
 
 app.listen(port, () => console.log(`Proxy listening on ${port}`));
